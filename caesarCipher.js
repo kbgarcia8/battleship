26f3311d62d8bc97849eead7578ba1f88d80c3c9
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-08-15 00:04:48 +0800

    minor progress

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,5 +1,19 @@
 function caesarCipher () {
-
+    const lowcaseStandard = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    const upcaseStandard = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    let shiftedLowcase = new Array(26).fill('');
+    let shitedUpcase = new Array(26).fill('');
+    const shift = 3;
+    //shift 2 alphabet array depending on shift
+    for(let i = shift-1,j =0; j<lowcaseStandard.length; i++,j++){
+        //console.log(lowcaseStandard[i])
+    }
+    shiftedLowcase[3] = lowcaseStandard[3]
+    console.log(lowcaseStandard[4])
+    console.log(shiftedLowcase)
+    //split string
+    //convert each char to respective index equivalent it standard arrays
+    //then retrieve same index number to shift then join then return encryted string
 }
-
-module.exports = caesarCipher;
+caesarCipher();
+//module.exports = caesarCipher;
--- caesarCipher.spec.js
+++ caesarCipher.spec.js
@@ -1,1 +1,15 @@
 const caesarCipher = require('./caesarCipher')
+
+describe('cesar cipher', () => {
+  test('return abc', () => {
+    expect(caesarCipher('xyz', 3)).toMatch('abc');
+  });
+
+  test('return KhOOr', () => {
+    expect(caesarCipher('HeLLo', 3)).toMatch('KhOOr');
+  });
+
+  test('return Khoor, Zruog!', () => {
+    expect(caesarCipher('Hello, World!', 3)).toMatch('Khoor, Zruog!');
+  });
+});
