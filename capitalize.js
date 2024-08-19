9259f14bcd31c6403e14facf43fe2a3bf363f4f6
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-08-19 23:32:16 +0800

    Adding initial files

--- capitalize.js
+++
@@ -1,8 +0,0 @@
-function capitalize(string) {
-  const re = /[a-zA-Z]/gm;
-  const firstChar = string.charAt(0);
-  if (firstChar.match(re)) return firstChar.toUpperCase();
-  else return "Please input a-zA-z as first character only";
-}
-
-module.exports = capitalize;
--- tester.spec.js
+++ tester.spec.js
@@ -1,8 +1,15 @@
-test('zero', () => {
-    const z = 0;
-    expect(z).not.toBeNull();
-    expect(z).toBeDefined();
-    expect(z).not.toBeUndefined();
-    expect(z).not.toBeTruthy();
-    expect(z).toBeFalsy();
+const test = require('./test')
+
+describe("tester", () => {
+  test("return the following array properties", () => {
+    const result = analyzeArray([1,8,3,4,2,6]);
+
+    // Check if the result equals the expected object
+    expect(result).toEqual({
+        average: 4,
+        min: 1,
+        max: 8,
+        length: 6
+     });
   });
+});
