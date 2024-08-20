e2862bfc03ba8850a1ccf3d664392f3737777014
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-08-20 23:04:49 +0800

    initial setup

---
+++ Ships.js
@@ -0,0 +1,5 @@
+function Ships () {
+    function createShip() {
+        
+    }
+}
---
+++ Ships.spec.js
@@ -0,0 +1,10 @@
+const Ships = require("./Ships");
+
+describe("ships processes", () => {
+  beforeEach(() => {
+    //initialize creation of all ship type
+  });
+  test("", () => {
+    //test rightness of ships creation
+  });
+});
