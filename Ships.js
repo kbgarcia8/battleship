fc93229001013ed1475b433c6ebd9dc031c1d089
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-08-22 01:18:08 +0800

    Initial ships factory

--- Ships.js
+++ Ships.js
@@ -1,5 +1,31 @@
-function Ships () {
-    function createShip() {
-        
-    }
+function Ships(name, length) {
+  return {
+    name,
+    length,
+    numHit: 0,
+    sunk: false,
+    orientation: 'vertical',    
+    position: [
+      [2, 1],
+      [2, 2],
+      [2, 3],
+      [2, 4],
+      [2, 5],
+    ],
+    hit(index) {
+      if (this.position[index] !== "X") {
+        this.position[index] = "X";
+        let targetHit = this.numHit + 1;
+        return (this.numHit = targetHit);
+      }
+      return;
+    },
+    isSunk() {
+      if (this.length == this.numHit) return true;
+
+      return false;
+    },
+  };
 }
+
+module.exports = Ships;
--- Ships.spec.js
+++ Ships.spec.js
@@ -1,10 +1,48 @@
-const Ships = require("./Ships");
+const Ships = require("./Ships.js");
 
-describe("ships processes", () => {
-  beforeEach(() => {
-    //initialize creation of all ship type
-  });
-  test("", () => {
-    //test rightness of ships creation
-  });
+beforeAll(() => {
+  return (ship = Ships("carrier", 5));
+});
+test("test creation of ship", () => {
+  const expectedObject = {
+    name: "carrier",
+    length: 5,
+    numHit: 0,
+    sunk: false,
+    orientation: 'vertical',
+    position: [
+      [2,1],
+      [2,2],
+      [2,3],
+      [2,4],
+      [2,5],
+    ],
+  };
+  expect(ship).toMatchObject(expectedObject);
+});
+
+test("test hit a ship position", () => {
+  const expectedObject = {
+    name: "carrier",
+    length: 5,
+    numHit: 1,
+    sunk: false,
+    orientation: 'vertical',    
+    position: [
+      [2,1],
+      [2,2],
+      [2,3],
+      [2,4],
+      [2,5],
+    ],
+  };
+  expect(ship.hit()).toEqual(expectedObject['numHit']);
+});
+
+test("test if ship is sunk", () => {
+  ship.hit(0)
+  ship.hit(1)
+  ship.hit(2)
+  ship.hit(3)
+  expect(ship.isSunk()).toBe(true);
 });
