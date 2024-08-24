55bfc5c5eec634a9b5e83f3dc946c2f0bb26f300
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2024-08-24 13:37:54 +0800

    upload right copy

--- tests/Gameboard.spec.js
+++ tests/Gameboard.spec.js
@@ -1,58 +1,123 @@
-const Ships = require("../factories/Ships");
+const Gameboard = require("../factories/Gameboard.js");
+const Ships = require("../factories/Ships.js");
 
-beforeAll(() => {
-  return (ship = new Ships("carrier", 4));
+beforeEach(() => {
+  gameboard = new Gameboard()
 });
-test("test creation of ship", () => {
-  const expectedObject = {
-    name: "carrier",
-    length: 5,
-    numHit: 0,
-    sunk: false,
-    orientation: "vertical",
-    position: [
-      [null, null],
-      [null, null],
-      [null, null],
-      [null, null],
-      [null, null],
-    ],
-  };
-  expect(ship).toMatchObject(expectedObject);
-});
-
-test("test hit a ship position", () => {
-  const expectedObject = {
-    name: "carrier",
-    length: 5,
-    numHit: 1,
-    sunk: false,
-    orientation: "vertical",
-    position: [
-      [null, null],
-      [null, null],
-      [null, null],
-      [null, null],
-      [null, null],
-    ],
-  };
-  expect(ship.hit()).toEqual(expectedObject["numHit"]);
-});
-
-test("test if ship is sunk", () => {
-  ship.hit(0);
-  ship.hit(1);
-  ship.hit(2);
-  ship.hit(3);
-  expect(ship.isSunk()).toBe(true);
-});
-
-test("test if ship is position is initialized", () => {
+//receiveAttack
+//missedAttack
+//If all ship is sunk = gameover
+//creation of blank gameboard
+test("test creation of gameboard", () => {
   const expectedObject = [
-    [null, null],
-    [null, null],
-    [null, null],
-    [null, null],
-  ];
-  expect(ship.initializePosition(ship.length)).toEqual(expectedObject);
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+  ]
+  expect(gameboard.gameboardArray).toMatchObject(expectedObject);
+});
+//initiliaze/create 5 ships for the game
+test("test initialization of ship", () => {
+  const expectedObject = {
+    gameboardArray: [
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null, null, null],
+    ],
+    shipsArray: [
+      {
+        name: "carrier",
+        length: 5,
+        numHit: 0,
+        sunk: false,
+        orientation: "vertical",
+        position: [
+          [null, null],
+          [null, null],
+          [null, null],
+          [null, null],
+          [null, null],
+        ],
+      },
+      {
+        name: "battleship",
+        length: 4,
+        numHit: 0,
+        sunk: false,
+        orientation: "vertical",
+        position: [
+          [null, null],
+          [null, null],
+          [null, null],
+          [null, null]
+        ],
+      },
+      {
+        name: "destroyer",
+        length: 3,
+        numHit: 0,
+        sunk: false,
+        orientation: "vertical",
+        position: [
+          [null, null],
+          [null, null],
+          [null, null]
+        ],
+      },
+      {
+        name: "submarine",
+        length: 3,
+        numHit: 0,
+        sunk: false,
+        orientation: "vertical",
+        position: [
+          [null, null],
+          [null, null],
+          [null, null]
+        ],
+      },
+      {
+        name: "patrol boat",
+        length: 2,
+        numHit: 0,
+        sunk: false,
+        orientation: "vertical",
+        position: [
+          [null, null],
+          [null, null]
+        ],
+      }
+    ]
+  }
+  expect(gameboard).toMatchObject(expectedObject);
+});
+//placing of created ship && checking if placement is valid
+test("test placement of ship on the gameboard", () => {
+  const expectedObject = [
+    ["carrier", "carrier", "carrier", "carrier", "carrier", null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+  ]
+  expect(gameboard.placeShip("carrier", [0, 0], 'horizontal')).toMatchObject(expectedObject);
 });
