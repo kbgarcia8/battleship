5176439afa5e1e3b8a614031657e5e120d1fc455
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2024-09-09 10:23:35 +0800

    clean up and continue to original index.js

--- src/factories/DOM.js
+++ src/factories/DOM.js
@@ -1,14 +1,3 @@
-import { Ships } from "../factories/Ships.js"
-import { Gameboard } from "../factories/Gameboard.js"
-import { Player } from "../factories/Player.js"
-import { Helper } from "../factories/Helper.js"
-
-const player1 = new Player("player")
-const helper = new Helper()
-const playerSpace = document.querySelector(".player-board")
-const playerAddShipSpace = document.querySelector(".ships-container")
-//console.log(player1.gameboard.gameboardArray)
-
 class DOM {
   constructor() {}
 
@@ -81,26 +70,6 @@ class DOM {
     })
   }
 }
-
-const createDOM = new DOM()
-createDOM.DOMGameboard(player1, playerSpace)
-createDOM.DOMAddShips(player1, playerAddShipSpace)
-
-const cells = document.querySelectorAll(".player-cell")
-const carrier = document.querySelector("#carrier")
-const battleship = document.querySelector("#battleship")
-const destroyer = document.querySelector("#destroyer")
-const submarine = document.querySelector("#submarine")
-const patrol_boat = document.querySelector("#patrol-boat")
-
-helper.dragShip(carrier)
-helper.dragShip(battleship)
-helper.dragShip(destroyer)
-helper.dragShip(submarine)
-helper.dragShip(patrol_boat)
-
-createDOM.DOMDropShips(cells)
-/*TEST FUNCTIONS*/
 
 
 
