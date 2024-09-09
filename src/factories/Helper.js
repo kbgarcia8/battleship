85c098c70ef59176e90c0509d9b25cc949edc601
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2024-09-09 13:11:38 +0800

    Added helper method IsAllShipsPlaced and startGame

--- src/factories/Helper.js
+++ src/factories/Helper.js
@@ -1,20 +1,36 @@
-export class Helper {
-    checkOrientation(element) {
-        let shipOrientation = null;
-        if (element.classList.contains("horizontal")) {
-          shipOrientation = "horizontal";
-        } else {
-          shipOrientation = "vertical";
-        }
-        return shipOrientation;
+class Helper {
+  checkOrientation(element) {
+      let shipOrientation = null;
+      if (element.classList.contains("horizontal")) {
+        shipOrientation = "horizontal";
+      } else {
+        shipOrientation = "vertical";
       }
-    dragShip(element) {
-        let currOrientation = this.checkOrientation(element)
-        const children = element.childNodes;
-        element.addEventListener("dragstart", (e) => {
-            e.dataTransfer.setData("textID", e.target.id)
-            e.dataTransfer.setData("textLength", children.length)
-            e.dataTransfer.setData("textOrientation", currOrientation)
+      return shipOrientation;
+    }
+  dragShip(element) {
+      let currOrientation = this.checkOrientation(element)
+      const children = element.childNodes;
+      element.addEventListener("dragstart", (e) => {
+          e.dataTransfer.setData("textID", e.target.id)
+          e.dataTransfer.setData("textLength", children.length)
+          e.dataTransfer.setData("textOrientation", currOrientation)
     })
-}
+  }
+  IsAllShipsPlaced(container) {
+    const containerChildren = container.childNodes
+    if(containerChildren.length == 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+  startGame(button,container,containerParent,AIContainer){
+    button.addEventListener("click", (e) => {
+      if(this.IsAllShipsPlaced(container)) {
+        containerParent.setAttribute("style", 'display: none;')
+        AIContainer.setAttribute("style", 'display: flex;')
+      }        
+    })
+  }
 }
