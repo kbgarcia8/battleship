adc0ac4c3c145d7bbc631441766bacf0e6c71118
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-09-09 23:43:34 +0800

    update to module from no ES6 script

--- src/factories/Helper.js
+++ src/factories/Helper.js
@@ -1,20 +1,46 @@
 export class Helper {
-    checkOrientation(element) {
-        let shipOrientation = null;
-        if (element.classList.contains("horizontal")) {
-          shipOrientation = "horizontal";
-        } else {
-          shipOrientation = "vertical";
-        }
-        return shipOrientation;
+  checkOrientation(element) {
+      let shipOrientation = null;
+      if (element.classList.contains("horizontal")) {
+        shipOrientation = "horizontal";
+      } else {
+        shipOrientation = "vertical";
       }
-    dragShip(element) {
-        let currOrientation = this.checkOrientation(element)
-        const children = element.childNodes;
-        element.addEventListener("dragstart", (e) => {
-            e.dataTransfer.setData("textID", e.target.id)
-            e.dataTransfer.setData("textLength", children.length)
-            e.dataTransfer.setData("textOrientation", currOrientation)
+      return shipOrientation;
+    }
+  dragShip(element) {
+      const children = element.childNodes;
+      element.addEventListener("dragstart", (e) => {
+          e.dataTransfer.setData("textID", e.target.id)
+          e.dataTransfer.setData("textLength", children.length)
     })
-}
+  }
+  IsAllShipsPlaced(container) {
+    const containerChildren = container.childNodes
+    if(containerChildren.length == 0) {
+      return true
+    } else {
+      return false
+    }
+  }
+  startGame(button,container,containerParent,AIContainer){
+    button.addEventListener("click", (e) => {
+      if(this.IsAllShipsPlaced(container)) {
+        containerParent.setAttribute("style", 'display: none;')
+        AIContainer.setAttribute("style", 'display: flex;')
+      }        
+    })
+  }
+  changeOrientation(element) {
+    element.addEventListener("dblclick", (e) => {
+      e.preventDefault()
+      if(element.classList.contains("horizontal")) {
+        element.classList.remove("horizontal")
+        element.classList.add("vertical")
+      } else if (element.classList.contains("vertical")) {
+        element.classList.remove("vertical")
+        element.classList.add("horizontal")
+      }
+    })
+  }
 }
