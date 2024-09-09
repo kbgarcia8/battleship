5ed673154f20e3383cccd3bc7591b655d6b9a4af
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2024-09-09 13:11:59 +0800

    Finalize DOMDropShips

--- src/factories/DOM.js
+++ src/factories/DOM.js
@@ -1,24 +1,24 @@
-class DOM {
-  constructor() {}
+export class DOM {
+  constructor(player, helper) {
+    this.player = player,
+    this.helper = helper
+  }
 
-  DOMGameboard(player, parent) {
-    for (let x = 0; x < player.gameboard.gameboardArray.length; x++) {
-      for (let y = 0; y < player.gameboard.gameboardArray[x].length; y++) {
+  DOMGameboard(parent) {
+    for (let x = 0; x < this.player.gameboard.gameboardArray.length; x++) {
+      for (let y = 0; y < this.player.gameboard.gameboardArray[x].length; y++) {
         var board_node = document.createElement("DIV")
-        board_node.setAttribute("class", `${player.name}-cell`)
+        board_node.setAttribute("class", `${this.player.name}-cell`)
         board_node.setAttribute("data-x", `${x}`)
         board_node.setAttribute("data-y", `${y}`)
-        board_node.setAttribute(
-          "data-in",
-          `${player.gameboard.gameboardArray[x][y]}`
-        )
+        board_node.setAttribute("data-in",`${this.player.gameboard.gameboardArray[x][y]}`)
         parent.appendChild(board_node)
       }
     }
   }
-  DOMAddShips(player, parent) {
-    for (let i = 0; i < player.gameboard.shipsArray.length; i++) {
-      let currShipName = player.gameboard.shipsArray[i].name;
+  DOMAddShips(parent) {
+    for (let i = 0; i < this.player.gameboard.shipsArray.length; i++) {
+      let currShipName = this.player.gameboard.shipsArray[i].name;
       const ship_container_node = document.createElement("DIV")
       ship_container_node.setAttribute(
         "class",
@@ -27,31 +27,28 @@ class DOM {
       ship_container_node.setAttribute("id", `${currShipName}`)
       ship_container_node.setAttribute("draggable", "true")
       parent.appendChild(ship_container_node)
-      for (let j = 0; j < player.gameboard.shipsArray[i].length; j++) {
+      for (let j = 0; j < this.player.gameboard.shipsArray[i].length; j++) {
         const ship_part_node = document.createElement("DIV")
         ship_part_node.setAttribute("class", `${currShipName}`)
-        //ship_part_node.setAttribute('id', `${currShipName}`)
         ship_container_node.appendChild(ship_part_node)
       }
     }
   }
-  DOMDropShips(cells) {
+  DOMDropShips(cells,container) {
     cells.forEach((cell) => {
       cell.addEventListener("dragover", (e) => {
         e.preventDefault()
       })
       cell.addEventListener("drop", (e) => {
         e.preventDefault()
-        console.log("dropped")
+        //console.log("dropped")
         let acquiredID = e.dataTransfer.getData("textID")
         let acquiredLength = e.dataTransfer.getData("textLength")
         let acquiredOrientation = e.dataTransfer.getData("textOrientation")
         let x = Number(cell.dataset.x)
         let y = Number(cell.dataset.y)
-        console.log([x, y])
-        console.log(cell.dataset, acquiredID, acquiredLength, acquiredOrientation)
-        if(player1.gameboard.placeShip(acquiredID, [x,y], acquiredOrientation)) {
-            console.log("possible")
+        if(this.player.gameboard.placeShip(acquiredID, [x,y], acquiredOrientation)) {
+            console.log("Placement is possible")
             cell.setAttribute("data-in", acquiredID)
             cell.classList.add(acquiredID)
             let i = 0;
@@ -62,7 +59,7 @@ class DOM {
               i++;
             }
             let element = document.querySelector(`.${acquiredID}-container`)
-            element.remove()
+            element.remove()       
         } else {
             alert("Invalid placement")
         }
