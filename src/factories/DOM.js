e610df361693c761766a9432a0021e39f33fceaf
kbgarcia8 <kbgarcia8@users.noreply.github.com>
2024-09-09 23:52:41 +0800

    finalize DOMDropShips

--- src/factories/DOM.js
+++ src/factories/DOM.js
@@ -1,133 +1,82 @@
-export class Gameboard {
-  constructor() {
-    this.gameboardArray = this.createGameboard()
-    this.shipsArray = this.initializeShipsArray()
-    this.missedAttacks = []
+export class DOM {
+  constructor(player, helper) {
+    this.player = player,
+    this.helper = helper
   }
-  createGameboard() {
-    let gameArray = [
-      [null, null, null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null, null, null],
-      [null, null, null, null, null, null, null, null, null, null],
-    ]
-    return gameArray
+
+  DOMGameboard(parent) {
+    for (let x = 0; x < this.player.gameboard.gameboardArray.length; x++) {
+      for (let y = 0; y < this.player.gameboard.gameboardArray[x].length; y++) {
+        var board_node = document.createElement("DIV")
+        board_node.setAttribute("class", `${this.player.name}-cell`)
+        board_node.setAttribute("data-x", `${x}`)
+        board_node.setAttribute("data-y", `${y}`)
+        board_node.setAttribute("data-in",`${this.player.gameboard.gameboardArray[x][y]}`)
+        parent.appendChild(board_node)
+      }
+    }
   }
-  
-  initializeShipsArray() {
-    const shipsArr = []
-    shipsArr.push(new Ships("carrier", 5))
-    shipsArr.push(new Ships("battleship", 4))
-    shipsArr.push(new Ships("destroyer", 3))
-    shipsArr.push(new Ships("submarine", 3))
-    shipsArr.push(new Ships("patrol-boat", 2))
-    return shipsArr
+  DOMAddShips(parent) {
+    for (let i = 0; i < this.player.gameboard.shipsArray.length; i++) {
+      let currShipName = this.player.gameboard.shipsArray[i].name;
+      const ship_container_node = document.createElement("DIV")
+      ship_container_node.setAttribute("class",`${currShipName}-container horizontal`)
+      ship_container_node.setAttribute("id", `${currShipName}`)
+      ship_container_node.setAttribute("draggable", "true")
+      parent.appendChild(ship_container_node)
+      for (let j = 0; j < this.player.gameboard.shipsArray[i].length; j++) {
+        const ship_part_node = document.createElement("DIV")
+        ship_part_node.setAttribute("class", `${currShipName}`)
+        ship_container_node.appendChild(ship_part_node)
+      }
+    }
   }
-  
-  placeShip(name, coords, orientation) {
-    if (this.isPlacementValid(name, coords, orientation) === true) {
-      const re = new RegExp(name, "g");
-      let currShipName = "";
-      let currShipLength = 0;
-      for (let i = 0; i < this.shipsArray.length; i++) {
-        if (this.shipsArray[i].name.match(re)) {
-          currShipName = this.shipsArray[i].name
-          currShipLength = this.shipsArray[i].length
-          this.shipsArray[i].orientation = orientation
-          const [x, y] = coords;
-          if (orientation == 'horizontal') {
-            for (let j = 0; j < currShipLength; j++) {
-              this.gameboardArray[x][y + j] = currShipName
-              this.shipsArray[i].position[j][0] = x
-              this.shipsArray[i].position[j][1] = y + j
+  DOMDropShips(cells) {
+    cells.forEach((cell) => {
+      cell.addEventListener("dragover", (e) => {
+        e.preventDefault()
+      })
+      cell.addEventListener("drop", (e) => {
+        e.preventDefault()        
+        let acquiredID = e.dataTransfer.getData("textID")
+        let acquiredLength = e.dataTransfer.getData("textLength")
+        let currentElement = document.querySelector(`#${acquiredID}`)
+        let acquiredOrientation = this.helper.checkOrientation(currentElement)
+        let x = Number(cell.dataset.x)
+        let y = Number(cell.dataset.y)
+        //console.log([x, y])
+        //console.log(cell.dataset, acquiredID, acquiredLength, acquiredOrientation)
+        const checkPlacement = this.player.gameboard.placeShip(acquiredID, [x,y], acquiredOrientation)
+        if(checkPlacement && acquiredOrientation == "horizontal") {
+            console.log("Horizontal placement is possible")
+            cell.setAttribute("data-in", acquiredID)
+            cell.classList.add(acquiredID)
+            let i = 0;
+            while (i < acquiredLength - 1) {
+              cell = cell.nextSibling;
+              cell.setAttribute("data-in", acquiredID)
+              cell.classList.add(acquiredID)
+              i++;
             }
-          } else if (orientation == 'vertical') {
-            for (let k = 0; k < currShipLength; k++) {
-              this.gameboardArray[x + k][y] = currShipName
-              this.shipsArray[i].position[k][0] = x + k
-              this.shipsArray[i].position[k][1] = y
-            }
+            let element = document.querySelector(`.${acquiredID}-container`)
+            element.remove()
+        } else if (checkPlacement && acquiredOrientation == "vertical") {//resolve else if why placeShip is being executed twice
+          console.log("Vertical placement is possible")
+          cell.setAttribute("data-in", acquiredID)
+          cell.classList.add(acquiredID)
+          let i = 0;
+          while (i < acquiredLength - 1) {
+            cell = cell.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling.nextSibling; //try the next 10 sibling
+            cell.setAttribute("data-in", acquiredID)
+            cell.classList.add(acquiredID)
+            i++;
           }
-          return {
-            "gameboardArray": this.gameboardArray,
-            "shipsArray": this.shipsArray,
-            "missedAttacks": this.missedAttacks
-          }
+          let element = document.querySelector(`.${acquiredID}-container`)
+          element.remove()
+        } else {
+            console.log("Invalid placement")
         }
-      }
-    } else {
-      return false
-    }
-  }
-  
-  isPlacementValid(name, coords, orientation) {
-    const re = new RegExp(name, "g");
-    let currShipLength = 0;
-    for (let i = 0; i < this.shipsArray.length; i++) {
-      if (this.shipsArray[i].name.match(re)) {
-        currShipLength = this.shipsArray[i].length
-      }
-    }
-    const [x, y] = coords;
-    
-    if (x > 10 || x < 0 || y > 10 || y < 0) return false
-    if (orientation == 'horizontal' && y + (currShipLength-1) < 10) {
-      let pathContent = []
-      for (let j = 0; j < currShipLength; j++) {
-        pathContent.push(this.gameboardArray[x][y + j])          
-      }
-      const filtered = pathContent.filter((content) => {return content == null})
-      if (filtered.length == pathContent.length) return true
-      return false
-    } else if (orientation == 'vertical' && x + (currShipLength-1) < 10) {
-      let pathContent = []
-      for (let k = 0; k < currShipLength; k++) {
-        pathContent.push(this.gameboardArray[x + k][y])
-      }
-      const filtered = pathContent.filter((content) => {return content == null})
-      if (filtered.length == pathContent.length) return true
-      return false
-    } else {
-      return false
-    }
-  }
-  
-  receiveAttack(coords) {
-    let currShipPosIndexHit = 0;
-    const [x, y] = coords;
-    if (this.gameboardArray[x][y] === null) {
-      this.gameboardArray[x][y] = "miss"
-      this.missedAttacks.push([x, y])
-    } else if (this.gameboardArray[x][y] !== null) {
-      let currentShipHit = this.gameboardArray[x][y]
-      let re = new RegExp(currentShipHit, "g");
-      for (let i = 0; i < this.shipsArray.length; i++) {
-        if (this.shipsArray[i].name.match(re)) {
-          currShipPosIndexHit = this.shipsArray[i]['position'].findIndex(pos => pos[0] === x && pos[1] === y)
-          this.shipsArray[i].hit(currShipPosIndexHit)
-        }
-      }
-      this.gameboardArray[x][y] = "X"      
-    } else {
-      return false
-    }
-    return {
-      "gameboardArray": this.gameboardArray,
-      "shipsArray": this.shipsArray,
-      "missedAttacks": this.missedAttacks
-    }
-  }
-  
-  isGameOver() {
-    for (let i = 0; i < this.shipsArray.length; i++) {
-      if (this.shipsArray[i].isSunk() === false) return false
-    }
-    return true
+      })
+    })
   }
 }
